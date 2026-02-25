0341d06b02dd66d86e556eceba6d8f77953f6357
WalterSouza-E <201622023@eniac.edu.br>
2026-02-25 18:57:10 -0300

    Adição de novas feeatures a tela de inicio

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -19,3 +19,44 @@ track.style.transform = 'translateX(${offset}%)';
 }
 
 setInterval(() => moveSlide(1), 5000);
+
+
+//animação depoimentos
+
+const elemento = document.getElementById('texto-animado');
+const frases = [
+  "Veja Alguns Depoimentos Dos Nossos Adotantes",
+  "Salve uma Vida",
+  "Alegre o coração de um pet"
+];
+
+let fraseIndex = 0;
+let caractereIndex = 0;
+let deletando = false;
+
+function animarTexto() {
+  const fraseAtual = frases[fraseIndex];
+  
+  if (deletando) {
+    elemento.textContent = fraseAtual.substring(0, caractereIndex - 1);
+    caractereIndex--;
+  } else {
+    elemento.textContent = fraseAtual.substring(0, caractereIndex + 1);
+    caractereIndex++;
+  }
+
+  let velocidade = deletando ? 50 : 100;
+
+  if (!deletando && caractereIndex === fraseAtual.length) {
+    velocidade = 2000;
+    deletando = true;
+  } else if (deletando && caractereIndex === 0) {
+    deletando = false;
+    fraseIndex = (fraseIndex + 1) % frases.length;
+    velocidade = 500;
+  }
+
+  setTimeout(animarTexto, velocidade);
+}
+
+animarTexto();
